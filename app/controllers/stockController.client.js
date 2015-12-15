ca4bdd1ee105cd831f751120e92aef6b0bcdf96f
Solms Dykman <solmsd@gmail.com>
2015-12-15 18:29:55 +0900

    Can now get stock info with YQL!

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -9,7 +9,7 @@
 			function ($scope, $http) {
 				$scope.stocks = [];
 
-				$scope.showStocks = function () {
+				var showStocks = function () {
 					$http.get('/api/stocks')
 						.success(function(response){
 							$scope.stocks = [];
@@ -18,12 +18,25 @@
 							}
 					});
 				}
-				$scope.showStocks();
+				showStocks();
 
 				$scope.addStock = function (to_add) {
+					var stock_obj = {
+						date: [],
+						close: []
+					};
+					getStock(to_add).success(function(data){
+						var results = data.query.results.quote;
+						console.log(results);
+						for (var i=0; i<results.length; i++){
+							stock_obj.date.push(results[i].Date);
+							stock_obj.close.push(results[i].Close);
+						}
+						console.log(stock_obj);
+					});
 					$http.post('/api/stocks?code=' + to_add)
 						.success(function(response){
-							$scope.showStocks();
+							showStocks();
 							console.log(response);
 						});
 				};
@@ -31,16 +44,13 @@
 				$scope.removeStock = function(to_delete){
 					$http.delete('/api/stocks?code=' + to_delete)
 						.success(function(response){
-							$scope.showStocks();
+							showStocks();
 							console.log(response);
 						});
 				}
 
-				$scope.searchBarchart = function(code){
-					$http.get('http://marketdata.websol.barchart.com/getQuote.json?key=c2d51bf69f6aacceb3fb55d333a0e7c8&symbols=ZC*1,IBM,GOOGL,^EURUSD')
-						.success(function(response){
-							console.log(response);
-						});
+				var getStock = function(symbol){
+					return $http.get('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "' + symbol + '" and startDate = "2015-12-10" and endDate = "2015-12-14"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys');
 				}
 		}]);
 })();
