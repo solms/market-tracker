9a40561e28d724f9cb0844cc2f594955f61e4047
Solms Dykman <solmsd@gmail.com>
2015-12-16 20:08:33 +0900

    Trying to implement D3js line graph with limited success

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -31,9 +31,10 @@
 							var results = data.query.results.quote;
 							for (var i=0; i<results.length; i++){
 								stock_obj.date.push(results[i].Date);
-								stock_obj.close.push(results[i].Close);
+								stock_obj.close.push(Number(results[i].Close));
 							}
 							console.log(stock_obj);
+							render(stock_obj);
 							$http.post('/api/stocks?code=' + to_add)
 								.success(function(response){
 									showStocks();
@@ -63,5 +64,34 @@
 
 					return $http.get('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "' + symbol + '" and startDate = "'+yql_start_date+'" and endDate = "'+yql_end_date+'"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys');
 				}
+
+				var render = function(data){
+					var outerWidth = 500;
+					var outerHeight = 300;
+					var margin = {top: 30, right: 30, bottom: 30, left: 30};
+					var xColumn = 'date';
+					var yColumn = 'close'; 	// Closing price
+
+					var svg = d3.select("body").append("svg")
+			      .attr("width",  outerWidth)
+			      .attr("height", outerHeight);
+					var g = svg.append('g');
+					var path = g.append('path');
+
+					var innerWidth  = outerWidth  - margin.left - margin.right;
+					var innerHeight = outerHeight - margin.top  - margin.bottom;
+
+					var xScale = d3.scale.linear().range([0, innerWidth]);
+					var yScale = d3.scale.linear().range([innerHeight, 0]);
+
+					var line = d3.svg.line()
+		        .x(function(d) { return xScale(d[xColumn]); })
+		        .y(function(d) { return yScale(d[yColumn]); });
+
+					xScale.domain( d3.extent(data, function (d){ return d[xColumn]; }));
+	        yScale.domain( d3.extent(data, function (d){ return d[yColumn]; }));
+	        path.attr("d", line(data));
+
+				};
 		}]);
 })();
