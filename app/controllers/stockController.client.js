157a043cacf32a942f76f8e753e1b84b008eeca7
Solms Dykman <solmsd@gmail.com>
2015-12-17 13:39:48 +0900

    Reformatted the graphing code a little

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -9,6 +9,13 @@
 			function ($scope, $http) {
 				$scope.stocks = [];
 
+				// Set up graphing area
+				var svg = d3.select("#graph-div").append("svg")
+					.attr("width",  500)
+					.attr("height", 300);
+				var xScale = d3.scale.linear().range([0, 500]);
+				var yScale = d3.scale.linear().range([0, 300]);
+
 				var showStocks = function () {
 					$http.get('/api/stocks')
 						.success(function(response){
@@ -32,12 +39,13 @@
 							var results = data.query.results.quote;
 							for (var i=0; i<results.length; i++){
 								stocks_arr.push({
+									code: to_add,
 									date: i,
 									close: Number(results[i].Close)
 								})
 							}
 							// Render the graph
-							render2(stocks_arr);
+							render(stocks_arr);
 							// Add the stock code to the database
 							$http.post('/api/stocks?code=' + to_add)
 								.success(function(response){
@@ -69,33 +77,17 @@
 					return $http.get('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "' + symbol + '" and startDate = "'+yql_start_date+'" and endDate = "'+yql_end_date+'"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys');
 				}
 
-				var render2 = function(dddd){
-					var svg = d3.select("#graph-div").append("svg")
-		        .attr("width",  500)
-		        .attr("height", 300);
+				var render = function(data){
+	        xScale.domain(d3.extent(data, function (d){ return d.date; }));
+	        yScale.domain(d3.extent(data, function (d){ return d.close; }));
 
-		      var xScale = d3.scale.linear().range([0, 500]);
-		      var yScale = d3.scale.linear().range([0, 300]);
+	        var circles = svg.selectAll("circle").data(data);
+	        circles.enter().append("circle").attr("r", 5);
+	        circles
+	          .attr("cx", function (d){ return xScale(d.date); })
+	          .attr("cy", function (d){ return yScale(d.close); });
 
-		      function render(data){
-
-						console.log('Made it in with:');
-						console.log(data);
-
-		        xScale.domain(d3.extent(data, function (d){ return d.date; }));
-		        yScale.domain(d3.extent(data, function (d){ return d.close; }));
-
-		        var circles = svg.selectAll("circle").data(data);
-		        circles.enter().append("circle").attr("r", 10);
-		        circles
-		          .attr("cx", function (d){ return xScale(d.date); })
-		          .attr("cy", function (d){ return yScale(d.close); });
-
-		        circles.exit().remove();
-		      }
-
-		      render(dddd);
-
-				};
+	        circles.exit().remove();
+	      };
 		}]);
 })();
