6e7620766342ed082bf48cd46fb4c34dc471ed45
Solms Dykman <solmsd@gmail.com>
2015-12-17 14:14:29 +0900

    Added the beginnings of axis...

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -8,6 +8,8 @@
 			'$http',
 			function ($scope, $http) {
 				$scope.stocks = [];
+				// Initialise stock object array
+				var stocks_arr = [];
 
 				// Set up graphing area
 				var outerWidth  = 500,
@@ -26,8 +28,15 @@
 				var innerWidth  = outerWidth  - margin.left - margin.right;
 	      var innerHeight = outerHeight - margin.top  - margin.bottom;
 
+				var xAxisG = g.append("g")
+        	.attr("transform", "translate(0," + innerHeight + ")");
+				var yAxisG = g.append("g");
+
 				var xScale = d3.scale.linear().range([0, innerWidth]);
 				var yScale = d3.scale.linear().range([0, innerHeight]);
+
+				var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+				var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
 				var line = d3.svg.line()
 					.x(function(d) { return xScale(d[xColumn]); })
@@ -45,7 +54,6 @@
 				showStocks();
 
 				$scope.addStock = function (to_add) {
-					var stocks_arr = [];
 					// Retrieve the stock data with Yahoo Query Language
 					// and add it to the database if successful
 					getStock(to_add).success(function(data){
@@ -94,10 +102,13 @@
 					return $http.get('http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "' + symbol + '" and startDate = "'+yql_start_date+'" and endDate = "'+yql_end_date+'"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys');
 				}
 
+				// Render the line graph
 				var render = function(data){
 	        xScale.domain(d3.extent(data, function (d){ return d[xColumn]; }));
 	        yScale.domain(d3.extent(data, function (d){ return d[yColumn]; }));
 					path.attr('d', line(data));
+					xAxisG.call(xAxis);
+					yAxisG.call(yAxis);
 	      };
 		}]);
 })();
