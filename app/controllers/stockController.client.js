d7f02d80da1df20efc7b3a517bc35ece256196c5
Solms Dykman <solmsd@gmail.com>
2015-12-18 17:48:56 +0900

    Graphs will be drawn when page is loaded

--- app/controllers/stockController.client.js
+++ app/controllers/stockController.client.js
@@ -46,12 +46,33 @@
 	        .x(function(d) { return xScale(d[xColumn]); })
 	        .y(function(d) { return yScale(d[yColumn]); });
 
+				// Add the stocks in the database to the $scope.stocks variable - this
+				// allows the selected stocks to be displayed.
+				// Also update the graph.
 				var showStocks = function () {
 					$http.get('/api/stocks')
 						.success(function(response){
 							$scope.stocks = [];
 							for(var i=0; i<response.length; i++){
 								$scope.stocks.push(response[i]);
+							}
+							// Update the local array for graphing the stocks
+							for(var i=0; i<$scope.stocks.length; i++){
+								// Get the info for each stock from YQL finance, and then update
+								// the local array of stock objects for graphing
+								getStockInfo($scope.stocks[i].code)
+									.success(function(data){
+										// Redundant, but check that the data exists anyway
+										var results = data.query.results.quote;
+										if(results != null){
+											console.log('Successfully got data via YQL.');
+											addStockToGraphArray(results);
+											render(stocks_arr);
+										}
+										else{
+											console.log('Could not retrieve stock data from YQL.');
+										}
+									});
 							}
 					});
 				}
@@ -60,37 +81,17 @@
 				$scope.addStock = function (to_add) {
 					// Retrieve the stock data with Yahoo Query Language
 					// and add it to the database if successful
-					getStock(to_add).success(function(data){
+					getStockInfo(to_add).success(function(data){
 						// Check if info on the specified stock code is found
-						if(data.query.results != null){
+						var results = data.query.results.quote;
+						if(results != null){
 							console.log('Successfully got data via YQL.');
-							// Create an array of stock objects to use for graphing
-							var results = data.query.results.quote;
-							var stock_values = [];
-							for (var i=0; i<results.length; i++){
-								stock_values.push({
-									date: i,
-									close: Number(results[i].Close)
-								})
-							}
-							stocks_arr.push({
-								name: to_add,
-								values: stock_values
-							});
-							console.log(stocks_arr);
-							render(stocks_arr);
-							/*
-							// Render the graph
-							var path = g.append('path');
-							render(path, stocks_arr);*/
-							// Add the stock code to the database
 							$http.post('/api/stocks?code=' + to_add)
 								.success(function(response){
 									showStocks();
-									console.log(response);
 								});
 							} else {
-								console.log('Could not find stock on YQL.');
+								console.log('Could not retrieve stock data from YQL.');
 							}
 					});
 				};
@@ -103,7 +104,8 @@
 						});
 				}
 
-				var getStock = function(symbol){
+				// Retrieve the stock data from YQL finance
+				var getStockInfo = function(symbol){
 					var date 						= new Date();
 					var yql_end_date 		= date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate();
 					console.log('End date: ' + yql_end_date);
@@ -120,17 +122,27 @@
 					g.selectAll("path").remove();
 
 					// Determine the domain
-					var max = 0;
+					var max = 0,
+							min = 0;
 					for(var i=0; i<data.length; i++){
-						var temp = d3.max(data[i].values, function (d) { return d[yColumn] });
-						if(temp>max){
-							max = temp;
+						var curr_max = d3.max(data[i].values,
+																	function (d) { return d[yColumn] });
+						if(curr_max>max){
+							if(max == 0){
+								min = max;
+							}
+							max = curr_max;
+						}
+						var curr_min = d3.min(data[i].values,
+																	function (d) { return d[yColumn] });
+						if(curr_min<min){
+							min = curr_min;
 						}
 					}
 
 					// Set the domain
 					xScale.domain([0, 25]);
-	        yScale.domain([0, max]);
+	        yScale.domain([min, max]);
 
 					for(var i=0; i<data.length; i++){
 						var path = g.append("path");
@@ -144,5 +156,27 @@
 					yAxisG.call(yAxis);
 					*/
 	      };
+
+				/* 	Populate a local array with stock information to be used for graphing
+				 		Format:
+						stocks_arr = [Object, Object, Object]
+							where Object = {
+								name: 'stock_symbol'
+								values: [{date: 'date', close: closing_price}, ...]
+							}
+				*/
+				var addStockToGraphArray = function(results){
+					var stock_values = [];
+					for (var i=0; i<results.length; i++){
+						stock_values.push({
+							date: i,
+							close: Number(results[i].Close)
+						})
+					}
+					stocks_arr.push({
+						name: results[0].Symbol,
+						values: stock_values
+					});
+				}
 		}]);
 })();
